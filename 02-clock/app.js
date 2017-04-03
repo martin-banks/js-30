ae01f30330af2ad8d87bda44278b939686be9583
Martin Banks <anotherbanksy@gmail.com>
2017-04-03 21:04:31 +0100

    refactor to update position instaead of re-rendering. add ubiz bezier transition

--- 02-clock/app.js
+++ 02-clock/app.js
@@ -3,7 +3,7 @@
 const state = {}
 
 function Clock() {
-	const time = ()=> new Date()
+	/*const time = ()=> new Date()
 	const hours = ()=> time().getHours()
 	const minutes = ()=> time().getMinutes()
 	const seconds = ()=> time().getSeconds()
@@ -13,7 +13,7 @@ function Clock() {
 		return (((newHours / 12) * 360) - 90) + (minutes()/60 * 12)
 	}
 	const calcMins = ()=>  (( (minutes() / 60) * 360)) - 90
-	const calcSecs = ()=> (((seconds() / 60) * 360)) - 90 
+	const calcSecs = ()=> (((seconds() / 60) * 360)) - 90 */
 	const ticks = ()=> {
 		let allTicks = []
 		for(let i = 0; i < 12; i++){
@@ -24,18 +24,54 @@ function Clock() {
 	const render = () => `<div class="clock">
 		<div class="face">
 			${ticks()}
-			<div class="hand second" style="transform: rotate(${calcSecs()}deg)"></div>
-			<div class="hand hour" style="transform: rotate(${calcHours()}deg)"></div>
-			<div class="hand minute" style="transform: rotate(${calcMins()}deg)"></div>	
+			<div class="hand second-hand" ></div>
+			<div class="hand hour-hand" ></div>
+			<div class="hand minute-hand" ></div>	
 		</div>	
 	</div>`
 
 	return { render }
 }
 
-const myClock = Clock()
-const loadClock = setInterval(()=>{
-	document.querySelector('#app').innerHTML = myClock.render()
+function setTime() {
+	const time = ()=> new Date()
+	const hours = ()=> time().getHours()
+	const minutes = ()=> time().getMinutes()
+	const seconds = ()=> time().getSeconds()
+
+	function calcHours(){
+		const newHours = hours() > 12 ? hours() - 12 : hours()
+		return (((newHours / 12) * 360) - 90) + (minutes()/60 * 12)
+	}
+	const calcMins = ()=>  (( (minutes() / 60) * 360)) - 90
+	const calcSecs = ()=> (((seconds() / 60) * 360)) - 90 
+	return {
+		calcHours,
+		calcMins,
+		calcSecs
+	}
+}
+
+const time = setTime()
+const myClock = Clock({
+	hours:time.calcHours(),
+	minutes: time.calcMins(),
+	seconds: time.calcSecs()
+})
+const setTimeValues = () => {
+	const hourHand = document.querySelector('.hour-hand')
+	const minuteHand = document.querySelector('.minute-hand')
+	const secondHand = document.querySelector('.second-hand')
+
+	hourHand.style.transform = `rotate(${time.calcHours()}deg)`
+	minuteHand.style.transform = `rotate(${time.calcMins()}deg)`
+	secondHand.style.transform = `rotate(${time.calcSecs()}deg)`
+}
+
+
+document.querySelector('#app').innerHTML = myClock.render()
+const updateTime = setInterval(()=>{
+	setTimeValues()
 }, 1000)
 
 
