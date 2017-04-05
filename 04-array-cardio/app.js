757f1c8e8a4354f7a87f66a49716ca6782b80af6
Martin Banks <anotherbanksy@gmail.com>
2017-04-05 21:41:13 +0100

    udpate with notes from wes bos solutions

--- 04-array-cardio/app.js
+++ 04-array-cardio/app.js
@@ -22,6 +22,9 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 const bornIn1500s = inventors.filter(inventor => {
 	if (inventor.year >= 1500 && inventor.year <= 1599) return true
 })
+/*
+const bornIn1500s = inventors.filter(inventor => inventor.year >= 1500 && inventor.year <= 1599)
+*/
 console.log('1. Born in the 1500s')
 console.table( bornIn1500s )
 
@@ -34,81 +37,64 @@ console.log('2. Array of inventor names\n', inventorNames)
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-const sortedByYear = inventors.sort((a,b)=>{
-	return a.year - b.year
-})
+//const sortedByYear = inventors.sort( (a,b) => a.year - b.year )
+const sortedByYear = inventors.sort( (a,b) => a.year > b.year ? 1 : -1 )
 console.log('3. Sort by year, oldest first')
 console.table(sortedByYear)
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
-const totalYears = inventors.reduce((total,inventor) =>{
+const totalYears = inventors.reduce((total,inventor) => {
 	const {year, passed} = inventor
-	let years = passed - year
-	return total + years
-},0)
+	return total + (passed - year)
+}, 0)
 console.log('4. total years lived', totalYears)
 
 
 // 5. Sort the inventors by years lived
 const sortedYearsLived = inventors.sort( (a,b)=>{
-	return (a.passes - a.year) - (b.passed - b.year)	
+	//return (a.passed - a.year) - (b.passed - b.year)	
+	// change -1 or for different orders
+	// a > b returns -1 will order by highest first 
+	return (a.passed - a.year) > (b.passed - b.year) ? -1 : 1
 })
 console.log('5. Sorted by years lived')
 console.table(sortedYearsLived)
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-const boulevards = [
-	`Boulevards of Paris`,
-	`City walls of Paris`,
-	`Thiers wall`,
-	`Wall of Charles V`,
-	`Wall of Philip II Augustus`,
-	`City gates of Paris`,
-	`Haussmann's renovation of Paris`,
-	`Boulevards of the Marshals`,
-	`Boulevard Auguste-Blanqui`,
-	`Boulevard Barbès`,
-	`Boulevard Beaumarchais`,
-	`Boulevard de l'Amiral-Bruix`,
-	`Boulevard des Capucines`,
-	`Boulevard de la Chapelle`,
-	`Boulevard de Clichy`,
-	`Boulevard du Crime`,
-	`Boulevard Haussmann`,
-	`Boulevard de l'Hôpital`,
-	`Boulevard des Italiens`,
-	`Boulevard de la Madeleine`,
-	`Boulevard de Magenta`,
-	`Boulevard Montmartre`,
-	`Boulevard du Montparnasse`,
-	`Boulevard Raspail`,
-	`Boulevard Richard-Lenoir`,
-	`Boulevard de Rochechouart`,
-	`Boulevard Saint-Germain`,
-	`Boulevard Saint-Michel`,
-	`Boulevard de Sébastopol`,
-	`Boulevard de Strasbourg`,
-	`Boulevard du Temple`,
-	`Boulevard Voltaire`,
-	`Boulevard de la Zone`
-]
-const boulevardDe = boulevards.filter(boulevard => {
-	return boulevard.split(' ').indexOf('de') !== -1 ? true : false
-})
+/*const boulevards = [
+	`Boulevards of Paris`,`City walls of Paris`,`Thiers wall`,`Wall of Charles V`,`Wall of Philip II Augustus`,`City gates of Paris`,`Haussmann's renovation of Paris`,`Boulevards of the Marshals`,`Boulevard Auguste-Blanqui`,`Boulevard Barbès`,`Boulevard Beaumarchais`,`Boulevard de l'Amiral-Bruix`,`Boulevard des Capucines`,`Boulevard de la Chapelle`,`Boulevard de Clichy`,`Boulevard du Crime`,`Boulevard Haussmann`,`Boulevard de l'Hôpital`,`Boulevard des Italiens`,`Boulevard de la Madeleine`,`Boulevard de Magenta`,`Boulevard Montmartre`,`Boulevard du Montparnasse`,`Boulevard Raspail`,`Boulevard Richard-Lenoir`,`Boulevard de Rochechouart`,`Boulevard Saint-Germain`,`Boulevard Saint-Michel`,`Boulevard de Sébastopol`,`Boulevard de Strasbourg`,`Boulevard du Temple`,`Boulevard Voltaire`,`Boulevard de la Zone`
+]*/
 
-console.log(`6. boulevards with 'de' in them\n`, boulevardDe)
+const boulevards = document.querySelectorAll('.mw-category li')
+const boulevardsWithDe = Array.from(boulevards)
+	.map(boulevard => boulevard.innerText)
+	.filter(boulevard => boulevard.includes('de'))
+
+console.log(`6. boulevards with 'de' in them\n`, boulevardsWithDe)
+
+// const boulevardsDe = boulevardText.filter(boulevard => {
+// 	return boulevard.split(' ').indexOf('de') !== -1 ? true : false
+// })
+
 
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 const namesSorted = people.sort((a,b)=>{
-	let first = (a.split(', ')[1].toUpperCase())
+	/*let first = (a.split(', ')[1].toUpperCase())
 	let second = (b.split(', ')[1].toUpperCase())
 	if (first < second) return -1
 	if (first > second) return 1
-	return 0
+	return 0*/
+
+	// Wes Bos solution
+	// when splitting a string the results can be destructured into named values
+	let [secondNameA, firstNameA] = a.split(', ')
+	let [secondNameB, firstNameB] = b.split(', ')
+	if (secondNameA === secondNameB) return 0
+	return secondNameA > secondNameB ? 1 : -1
 })
 console.log('7. People sorted by surname\n', namesSorted)
 
