fee04acd921a3e1ed5da3751d981f775c4b55183
Martin Banks <anotherbanksy@gmail.com>
2017-04-09 21:21:43 +0100

    complete konami code

---
+++ 12-Key-sequence-direction/app.js
@@ -0,0 +1,30 @@
+"use strict"
+let sequence = [	
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowLeft',
+	'ArrowRight',
+	'ArrowLeft',
+	'ArrowRight',
+	'b',
+	'a',
+	'Enter'
+]	
+let pressed = []
+
+window.addEventListener('keyup', e => {
+	pressed.push(e.key)
+	if (pressed.length > sequence.length) pressed.shift()
+	console.log(pressed)
+
+	if(pressed.join('').toLowerCase() === sequence.join('').toLowerCase() ) {
+		console.log('%cKONAMI CODE!!!', 'font-size: 50px')
+	}
+
+	/*if( pressed.join('').toLowerCase().includes(sequence.join('').toLowerCase() )) {
+		console.log('%cKONAMI CODE!!!', 'font-size: 50px')
+	}*/
+	
+})
